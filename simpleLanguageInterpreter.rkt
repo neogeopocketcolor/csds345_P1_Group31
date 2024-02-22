476f7b8adfa4c15376a23cda4a6be675acc5bc5a
neogeopocketcolor <107699399+neogeopocketcolor@users.noreply.github.com>
2024-02-21 22:02:24 -0500

    Final check!

--- simpleLanguageInterpreter.rkt
+++ simpleLanguageInterpreter.rkt
@@ -1,16 +1,18 @@
 #lang racket
 (require "simpleParser.rkt")
 
-
 #|
 
-To Do List:
-Connect Parser to allow for file interpretation
+Alex Seidman - Avry Rechel - Shea Leech
+ads206 - ajr250 - csl86
+2/21/2024
+CSDS 345
+Project 1 - Simple Language Interpreter
 
 |#
 
 ;Abstractions
-(define comand caar)
+(define command caar)
 (define statement car)
 (define nextStatement cdr)
 (define body cddar)
@@ -30,46 +32,45 @@ Connect Parser to allow for file interpretation
 (define returnVal cadr)
 (define returnify (lambda (v) (list 'return v)))
  
-;interpret command
+;interpret command - required, parses the input file and executes the code.
 (define interpret
   (lambda (filename)
     (M-state (parser filename) '())))
 
-;returns stateList -- the state represented by a list
-(define M-state ;returns stateList
+;M-state - updates the stateList based on the current command at the front of the list.
+(define M-state
   (lambda (lis stateList)
     (cond
       [(null? lis) stateList]
-      [(eq? (comand lis) '=)      (M-state (nextStatement lis) (M-assign (statement lis) stateList))]
-      [(eq? (comand lis) 'var)    (M-state (nextStatement lis) (M-declare (statement lis) stateList))]
-      [(eq? (comand lis) 'if)     (M-state (nextStatement lis) (if (M-boolean (condition lis) stateList)
+      [(eq? (command lis) '=)      (M-state (nextStatement lis) (M-assign (statement lis) stateList))]
+      [(eq? (command lis) 'var)    (M-state (nextStatement lis) (M-declare (statement lis) stateList))]
+      [(eq? (command lis) 'if)     (M-state (nextStatement lis) (if (M-boolean (condition lis) stateList)
                                                                    (M-state (statement1 lis) stateList)
                                                                    (if (not (null? (M-else lis)))
                                                                        (M-state (statement2 lis) stateList)
                                                                        stateList)))]
-      [(eq? (comand lis) 'while)  (if (M-boolean (condition lis) stateList)
+      [(eq? (command lis) 'while)  (if (M-boolean (condition lis) stateList)
                                           (M-state lis (M-state (body lis) stateList))
                                           (M-state (nextStatement lis) stateList))]
-      [(eq? (comand lis) 'return) (M-return (statement lis) stateList)]
-      [else 'placeholder]))) ;adjust this
+      [(eq? (command lis) 'return) (M-return (statement lis) stateList)]
+      [else (error 'Interpreter "Not a valid command")]))) 
 
-(define M-declare ;returns updated stateList
+;M-declare - declares a variable, either with or without a binding to a value.
+(define M-declare
   (lambda (lis stateList)
     (if (null? (value lis))
         (AddBinding (varValue lis) stateList) ;declare only
         (M-assign lis (AddBinding (varValue lis) stateList))))) ;declare and assign
         
-      
-
-(define M-assign ;returns updated stateList
+;M-assign - assigns a binding to a variable if the variable doesn't already have a value.
+(define M-assign 
   (lambda (lis stateList)
-    (cond
-      [(not (declared? (leftoperand lis) stateList)) (error 'Interpreter "Variable not declared. :(")]
-      ;[(eq? (CheckBinding (leftoperand lis) stateList) 'null) (error 'Interpreter "Variable not initialized.")]
-      [else (ChangeBinding (leftoperand lis) (M-expression (rightoperand lis) stateList) stateList)])))
+    (if (not (declared? (leftoperand lis) stateList))
+        (error 'Interpreter "Variable not declared. :(")
+        (ChangeBinding (leftoperand lis) (M-expression (rightoperand lis) stateList) stateList))))
        
-;
-(define M-expression ;returns number if math, boolean if not
+;M-expression - checks if an operation needs to return a number (math equation) or a boolean (t/f).
+(define M-expression
   (lambda (lis stateList)
     (cond
       [(not (list? lis)) (if (math? lis)
@@ -77,9 +78,9 @@ Connect Parser to allow for file interpretation
                              (M-boolean lis stateList))]
       [(declared? lis stateList) (M-expression (CheckBinding lis stateList) stateList)]
       [(math? (operator lis)) (M-integer lis stateList)]
-      [else (M-boolean lis stateList)]))) ;expressions can only be mathematical or boolean, might be a source of bugs (not checking if it is a boolean expression)
+      [else (M-boolean lis stateList)])))
 
-;tests if val is a number or math operator,  returning #t if it is, #f otherwise
+;math? - tests if val is a number or math operator, returning #t if it is, #f otherwise.
 (define math?
   (lambda (val)
     (cond
@@ -91,8 +92,7 @@ Connect Parser to allow for file interpretation
       [(eq? '% val) #t]
       [else #f])))
 
-;declared? takes a var name and the stateList, returning #t if var name exists in statelist. ex: (declared? 'x ((x 3))) returns #t
-
+;declared? - takes a var name and the stateList, returning #t if var name exists in statelist. ex: (declared? 'x ((x 3))) returns #t.
 (define declared?
   (lambda (var stateList)
     (cond
@@ -100,15 +100,15 @@ Connect Parser to allow for file interpretation
       ((equal? (variableDec stateList) var) #t)
       (else (declared? var (cdr stateList))))))
 
-;AddBinding takes a var name and the statelist, creates a new binding with given var 
-(define AddBinding ;returns updated stateList
+;AddBinding - takes a var name and the statelist, creates a new binding with given var.
+(define AddBinding
   (lambda (var stateList)
     (if (declared? var stateList)
         (error 'Interpreter "Variable already declared.")
         (cons (list var 'null) stateList))))
 
 
-;CheckBinding takes a var name and statelist, then returns the value of the variable
+;CheckBinding - takes a var name and statelist, then returns the value of the variable.
 (define CheckBinding
   (lambda (var stateList)
     (cond
@@ -117,7 +117,7 @@ Connect Parser to allow for file interpretation
       (else (CheckBinding var (cdr stateList))))))
     
 
-;ChangeBinding takes a var name, value, and stateList, then returns the stateList with the new variable value,
+;ChangeBinding - takes a var name, value, and stateList, then returns the stateList with the new variable's value updated.
 (define ChangeBinding
   (lambda (var newVal stateList)
     (cond
@@ -127,8 +127,8 @@ Connect Parser to allow for file interpretation
       (else
        (cons (car stateList) (ChangeBinding var newVal (cdr stateList)))))))
 
-
-(define M-integer ;returns a number
+;M-integer - checks what kind of operation needs to be performed, returns an integer.
+(define M-integer
   (lambda (lis stateList)
     (cond
       [(number? lis) lis]
@@ -141,7 +141,8 @@ Connect Parser to allow for file interpretation
       [(eq? (operator lis) '%) (remainder (M-integer (leftoperand lis) stateList) (M-integer (rightoperand lis) stateList))]
       [else (error 'Interpreter "M-integer_Error")])))
 
-(define M-boolean ;returns #t or #f
+;M-boolean - checks what kind of comparison must be made, returns either #t or #f.
+(define M-boolean
   (lambda (lis stateList)
     (cond
       [(eq? lis 'true) #t]
@@ -159,8 +160,7 @@ Connect Parser to allow for file interpretation
       [else (error 'Interpreter "M-boolean_Error")])))
       
 
-;return statement
-
+;M-return - prints out the requested return value. Makes sure that #t/#f becomes 'true and 'false as well.
 (define M-return
   (lambda (statement stateList)
     (cond
@@ -172,16 +172,6 @@ Connect Parser to allow for file interpretation
       ((declared? (returnVal statement) stateList) (M-return (returnify (CheckBinding (returnVal statement) stateList)) stateList)) ;check if statement is a declared variable, if so return the value.
       (else (error 'Interpreter "M-return error")))))
 
-;Testing code
-
-(interpret "test.txt") ;rename this to whatever you need
-
-;(M-state '((return 150)) '((A 1) (B 2) (C 3)))
-;(M-state '((return (- (/ (* 6 (+ 8 (% 5 3))) 11) 9))) '((A 1) (B 2) (C 3)))
-;(M-state '((return C)) '((A 1) (B 2) (C 3)))
-;(M-state '((return (* C B))) '((A 1) (B 2) (C 3)))
-;(M-state '((var z) (= z 10) (return z)) '()) ;Broken
-
-
+;END
 
  
